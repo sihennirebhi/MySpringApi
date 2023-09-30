3c5a6fcd901e25c3b4280ebb443939c5b8003d53
sihenni <r.s.henni@live.fr>
2023-09-30 12:12:53 +0100

    Sorry @Autowired, to correct the last commit

--- MySpringApi/src/main/java/com/sihenni/MySpringApi/App.java
+++ MySpringApi/src/main/java/com/sihenni/MySpringApi/App.java
@@ -3,10 +3,7 @@ package com.sihenni.MySpringApi;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
